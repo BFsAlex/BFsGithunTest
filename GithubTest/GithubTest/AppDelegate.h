ca17bfd3a3a3ca63e9a46cceb1b316c314bcf1a1
BFsAlex <838318741@qq.com>
2019-09-26 16:46:47 +0800

    Github test

---
+++ GithubTest/GithubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubTest
+//
+//  Created by 刘玲 on 2019/9/26.
+//  Copyright © 2019年 BFs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubTest/GithubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubTest
+//
+//  Created by 刘玲 on 2019/9/26.
+//  Copyright © 2019年 BFs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
